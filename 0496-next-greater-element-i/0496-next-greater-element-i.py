eada71a36bbf1a135ea5a2ccdb6a24218fda2abe
Aishwarya Tolani <iaishwaryatolani04@gmail.com>
2026-08-27 06:21:55 +0530

    Time: 0 ms (100%), Space: 19.3 MB (69.15%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,14 +1,47 @@
 class Solution:
+
+    # BRUTE FORCE
+
+
+    # def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    #     ans=[]
+    #     for i in range (len(nums1)):
+    #         maxnum=-1
+    #         for j in range (len(nums2)):
+    #             if nums1[i]==nums2[j]:
+    #                 for k in range(j+1,len(nums2)):
+    #                     if nums2[k]>nums2[j] :
+    #                         maxnum=nums2[k]
+    #                         break
+    #         ans.append(maxnum)
+    #     return ans
+
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans=[]
-        for i in range (len(nums1)):
-            maxnum=-1
-            for j in range (len(nums2)):
-                if nums1[i]==nums2[j]:
-                    for k in range(j+1,len(nums2)):
-                        if nums2[k]>nums2[j] :
-                            maxnum=nums2[k]
-                            break
-            ans.append(maxnum)
+        mydict = {}
+        stack = []
+        ans = []
+
+        for i in range(len(nums2) - 1):
+            stack.append(nums2[i])
+
+            while stack and nums2[i + 1] > stack[-1]:
+                mydict[stack[-1]] = nums2[i + 1]
+                stack.pop()
+
+        while stack:
+            mydict[stack[-1]] = -1
+            stack.pop()
+
+        mydict[nums2[-1]] = -1
+
+        for i in range(len(nums1)):
+            ans.append(mydict[nums1[i]])
+
         return ans
+
+
+
+    
+
+
             
