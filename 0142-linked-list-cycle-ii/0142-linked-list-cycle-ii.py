b54fab183c40de4bc99b315ad69b3e4b8079c5bb
Aishwarya Tolani <iaishwaryatolani04@gmail.com>
2026-09-06 01:11:37 +0530

    Time: 52 ms (61.7%), Space: 22.4 MB (42.25%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -17,6 +17,7 @@ class Solution:
                 while slow!=fast:
                     slow=slow.next
                     fast=fast.next
+                
                 return slow
         
         
