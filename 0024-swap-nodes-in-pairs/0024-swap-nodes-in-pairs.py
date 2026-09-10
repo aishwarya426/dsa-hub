90a7084778ba351dfda4319c8cd4ff95174ab9d2
Aishwarya Tolani <iaishwaryatolani04@gmail.com>
2026-09-10 06:39:36 +0530

    Time: 0 ms (100%), Space: 19.3 MB (61.95%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -1,71 +1,82 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head==None:
-            return None
-        count=0
-        curr=head
-        while curr :
-            curr=curr.next
-            count+=1
-        if count==1:
-            return head
+#         if head==None:
+#             return None
+#         count=0
+#         curr=head
+#         while curr :
+#             curr=curr.next
+#             count+=1
+#         if count==1:
+#             return head
 
-        if count ==2:
-            left=head
-            right=head.next
-            right.next=left
-            head=right
-            left.next=None
-            return head
+#         if count ==2:
+#             left=head
+#             right=head.next
+#             right.next=left
+#             head=right
+#             left.next=None
+#             return head
             
-        if count%2==0 and count!=2:
-            left =head
-            right=head.next
-            x=right.next
-            right.next=left
-            y=left
-            head=right
-            left=x
-            right=left.next
-            y.next=right
-            while right.next and right.next.next:
-                x=right.next
-                right.next=left
-                y=left
-                left=x
-                right=left.next
-                y.next=right
-            right.next=left
-            left.next=None
-        else:
-            left =head
-            right=head.next
-            x=right.next
-            right.next=left
-            y=left
-            head=right
-            left=x
-            right=left.next
-            y.next=right
-            while right:
-                x=right.next
-                right.next=left
-                y=left
-                left=x
-                right=left.next
-                y.next=right
-            y.next=left
-            left.next=None
-            
-            
+#         if count%2==0 and count!=2:
+#             left =head
+#             right=head.next
+#             x=right.next
+#             right.next=left
+#             y=left
+#             head=right
+#             left=x
+#             right=left.next
+#             y.next=right
+#             while right.next and right.next.next:
+#                 x=right.next
+#                 right.next=left
+#                 y=left
+#                 left=x
+#                 right=left.next
+#                 y.next=right
+#             right.next=left
+#             left.next=None
+#         else:
+#             left =head
+#             right=head.next
+#             x=right.next
+#             right.next=left
+#             y=left
+#             head=right
+#             left=x
+#             right=left.next
+#             y.next=right
+#             while right:
+#                 x=right.next
+#                 right.next=left
+#                 y=left
+#                 left=x
+#                 right=left.next
+#                 y.next=right
+#             y.next=left
+#             left.next=None        
+#         return head
 
-                
-        return head
+        dumm=ListNode(0)
+        dumm.next=head
+        curr=dumm
+        while curr.next and curr.next.next:
+            first=curr.next
+            second=curr.next.next
+            first.next=second.next
+            second.next=first
+            curr.next=second
+            curr=first
+        return dumm.next
+
+
+
 
         
         
